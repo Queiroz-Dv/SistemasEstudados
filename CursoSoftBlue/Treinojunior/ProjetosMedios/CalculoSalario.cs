ba6ac339892fd2a385212518db1b64b86e15dab0
Queiroz <kalifor_dbel@hotmail.com>
2022-01-17 17:39:36 -0300

    Implementação da classe de estudo atualizado. Prosseguindo para implementação dos métodos finais.

--- CursoSoftBlue/Treinojunior/ProjetosMedios/CalculoSalario.cs
+++ CursoSoftBlue/Treinojunior/ProjetosMedios/CalculoSalario.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 namespace Treinojunior.ProjetosMedios
 {
     class CalculoSalario : MenuPrincipal
@@ -10,14 +9,14 @@ namespace Treinojunior.ProjetosMedios
             //Tamanho da tela
             Console.SetWindowSize(100, 20);
             //Nome de cima da tela
-            Console.Title = "Queiroz Sistemas[v1.0]";
+            Console.Title = "Cálculo Salario";
             Console.ForegroundColor = ConsoleColor.White;
         }
         public static int Menu()
         {
+
             while (true)
             {
-                
                 Console.Clear();
                 Centralizar("==== Bem Vindo ao Diário de Sistemas ====\n");
                 Console.BackgroundColor = ConsoleColor.DarkGray;
@@ -43,46 +42,159 @@ namespace Treinojunior.ProjetosMedios
                             Console.Clear();
                             break;
                         case 1:
+
                             // Executa os procedimentos
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Linha();
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, "|| Digite o salário mínimno:                     || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Linha();
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            double salarioMinimo = double.Parse(Console.ReadLine());
 
-                            if (true)
-                            {
-                                //Se algo não der certo volta ao menu
-                                Centralizar("Pressione qualquer tecla para voltrar ao menu principal.");
-                                Console.ReadKey();
-                                Console.Clear();
-                                continue;
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Linha();
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite o salário mínimno: {salarioMinimo}    || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, "|| Digite as horas trabalhadas:     || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Linha();
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            double horasTrabalhadas = double.Parse(Console.ReadLine());
 
-                            }
-                            else if (true)
+
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Linha();
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite o salário mínimno: {salarioMinimo}           || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite as horas trabalhadas: {horasTrabalhadas}        || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Linha();
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            string categoria = Categorias();
+
+
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Linha();
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite o salário mínimno: {salarioMinimo}         || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite as horas trabalhadas: {horasTrabalhadas}      || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(35, $"|| Digite o salário mínimno: {categoria}           || "));
+                            Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                            Linha();
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            string turno = Turnos();
+                            if (turno == "Manhã" || turno == "Tarde" || turno == "Noite")
                             {
-                                //Senão testa outro procedimento
-                                Centralizar("Pressione qualquer tecla para voltrar ao menu principal.");
-                                Console.ReadKey();
-                                Console.Clear();
+                                Centralizar("Opção inválida. Escolha os dados corretamente");
                                 continue;
                             }
                             else
                             {
-                                //Caso algo não saia como esperado
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
+                                ApresentarResultados();
+
                             }
-                        default:
-                            //Para opções fora de contexto
-                            Centralizar("Opção inválida");
-                            Console.ReadKey();
-                            Console.Clear();
-                            continue;
                     }
-                    //valor do menu de acesso
                     return valor;
                 }
                 catch (FormatException)
                 {
                     return 0;
                 }
+            }
+        }
+        private  void Processamento(string categorias, string turno, double horasTrabalhadas, double valorSalarioMinimo)
+        {
+            double valorCoeficiente = GetCoeficiente(turno);
+            double valorGratificacao = GetGratificacao(turno, horasTrabalhadas);
+            double valorSalarioBruto = horasTrabalhadas * valorCoeficiente;
+            double valorImposto = GetValorImposto(categorias, valorSalarioBruto);
+            double valorAuxilioAlimentacao = GetValorAuxilioAlimentacao(categorias, valorSalarioBruto, valorSalarioMinimo);
+            double valorSalarioLiquido = (valorSalarioBruto + (valorGratificacao + valorAuxilioAlimentacao)) - valorImposto;
+
+            ApresentarResultados(valorCoeficiente, valorSalarioBruto,
+                                 valorImposto,  valorGratificacao, 
+                                 valorAuxilioAlimentacao, valorSalarioLiquido);
+
+        }
+
+        private static void ApresentarResultados(double valorCoeficiente, double valorSalarioBruto, double valorImposto, double valorGratificacao, double valorAuxilioAlimentacao, double valorSalarioLiquido)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetValorAuxilioAlimentacao(string categorias, double valorSalarioBruto, double valorSalarioMinimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetValorImposto(string categorias, double valorSalarioBruto)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetGratificacao(string turno, double horasTrabalhadas)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetCoeficiente(string turno)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Turnos()
+        {
+            Centralizar("Escolha o turno: 1.Manhã | 2.Tarde | 3.Noite ");
+            int tr = int.Parse(Console.ReadLine());
+            if (tr == 1)
+            {
+                string turno;
+                string turnoManha = "Manhã";
+                return turno = turnoManha;
+            }
+            else if (tr == 2)
+            {
+                string turno;
+                string turnoTarde = "Tarde";
+                return turno = turnoTarde;
+            }
+            else if (tr == 3)
+            {
+                string turno;
+                string turnoNoite = "Noite";
+                return turno = turnoNoite;
+            }
+            else
+            {
+                string turno = "Error. Nenhuma das opções selecionadas.";
+                Centralizar(turno);
+                return turno;
+            }
+
+        }
+
+        private static string Categorias()
+        {
+            Centralizar("Escolha a categoria: 1.Calouro | 2.Veterano");
+            int categ = int.Parse(Console.ReadLine());
+            if (categ == 1)
+            {
+                string categoria;
+                string ct = "Calouro";
+                return categoria = ct;
+            }
+            else
+            {
+                string categoria;
+                string ct = "Veterano";
+                return categoria = ct;
+
             }
         }
     }

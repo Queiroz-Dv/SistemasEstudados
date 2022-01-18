a00fe394657c1387d77bd9f2dfcbc95eecc847d2
Queiroz <kalifor_dbel@hotmail.com>
2022-01-18 14:57:03 -0300

    Implementação dos métodos de processamento.

--- CursoSoftBlue/Treinojunior/Menus/MenuProjetosGrandes.cs
+++ CursoSoftBlue/Treinojunior/Menus/MenuProjetosGrandes.cs
@@ -1,12 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Treinojunior.ProjetosGrandes
 {
-    class MenuProjetosGrandes
+    class MenuProjetosGrandes : MenuPrincipal
     {
+        public MenuProjetosGrandes()
+        {
+            //Tamanho da tela
+            Console.SetWindowSize(100, 20);
+            //Nome de cima da tela
+            Console.Title = "Menu Projetos de Grande Porte";
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static int Menu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Centralizar("==== Bem Vindo ao Portfólio de Sistemas Queiroz ====\n");
+                Centralizar("Hoje é " + DateTime.Now.ToLongDateString());
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Linha();
+                Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                Console.WriteLine("|{0}|", AlinhaTexto(35, "|| 0. Voltar para projetos de pequeno porte <<        ||"));
+                Console.WriteLine("|{0}|", AlinhaTexto(35, "|| 1. Sistema de Consumo de Energia                   ||"));
+                Console.WriteLine("|{0}|", AlinhaTexto(0, ""));
+                Linha();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("\n{0}", AlinhaTexto(36, "Escolha uma das opções de estudos realizados: ", "L"));
+                try
+                {
+                    int valor = int.Parse(Console.ReadLine());
+                    switch (valor)
+                    {
+                        case 0:
+                            Console.Clear();
+                            break;
+                        case 1:
+                            ConsumoEnergia.Menu();
+                            continue;
+                        default:
+                            Centralizar("Opção inválida.");
+                            Console.ReadKey();
+                            continue;
+                    }
+                    return valor;
+                }
+                catch (FormatException)
+                {
+                    return 0;
+                }
+            }
+        }
     }
 }
---
+++ CursoSoftBlue/Treinojunior/ProjetosGrandes/SistemaConsumoEnergia/BaseLeitura.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoEnergiaCondominio
+{
+    class BaseLeitura
+    {
+        public string Casa { get; set; }
+        public double Consumo { get; set; }
+        public double Desconto { get { return Consumo * 0.20; } }
+
+        public BaseLeitura(string casa, double consumo)
+        {
+            this.Casa = casa;
+            this.Consumo = consumo;
+        }
+
+        public override bool Equals(Object l)
+        {
+            BaseLeitura leitura = l as BaseLeitura;
+            if (leitura == null)
+            {
+                return false;
+            }
+            return (Casa.Equals(leitura.Casa));
+        }
+        public override int GetHashCode()
+        {
+            return new { Casa, Consumo }.GetHashCode();
+        }
+    }
+}
+
+

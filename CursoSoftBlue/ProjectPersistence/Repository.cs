3e5b59b088ad3a574da95dec9936201c76aff994
Queiroz <kalifor_dbel@hotmail.com>
2022-01-21 18:13:47 -0300

    CRUD do repositório finalizado.

--- CursoSoftBlue/ProjectPersistence/Repository.cs
+++ CursoSoftBlue/ProjectPersistence/Repository.cs
@@ -36,5 +36,51 @@ namespace ProjectPersistence
             this.fornecedores[this.fornecedores.IndexOf(fornecedor)] = fornecedor;
             return fornecedor;
         }
+
+        //CRUD Produto
+        public Produto InsertProduto(Produto produto)
+        {
+            this.produtos.Add(produto);
+            return produto;
+        }
+
+        public void RemoverProduto(Produto produto)
+        {
+            this.produtos.Remove(produto);
+        }
+
+        public IList<Produto> GetAllProdutos()
+        {
+            return this.produtos;
+        }
+
+        public Produto UpdateProduto(Produto produto)
+        {
+            this.produtos[this.produtos.IndexOf(produto)] = produto;
+            return produto;
+        }
+
+        //CRUD Notas
+        public InputNota InsertNota(InputNota nota)
+        {
+            this.inputNotas.Add(nota);
+            return nota;
+        }
+
+        public void RemoverNotas(InputNota nota)
+        {
+            this.inputNotas.Remove(nota);
+        }
+
+        public IList<InputNota> GetAllNotas()
+        {
+            return this.inputNotas;
+        }
+
+        public InputNota UpdateNota(InputNota nota)
+        {
+            this.inputNotas[this.inputNotas.IndexOf(nota)] = nota;
+            return nota;
+        }
     }
 }

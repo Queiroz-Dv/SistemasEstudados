f7fc4d8f0014ca7051b7bec4bc11bf63210902fa
Queiroz <kalifor_dbel@hotmail.com>
2022-01-21 17:35:07 -0300

    Implementação e adição de novas camadas para os estudos de MVC em Windows Form.

---
+++ CursoSoftBlue/Treinojunior/ProjectModel/InputNota.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinojunior.ProjectModel
+{
+    class InputNota
+    {
+        public Guid ID { get; set; }
+        public string Numero { get; set; }
+        public Fornecedor FornecedorNota { get; set; }
+        public string CNPJ { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public IList<InputProdutoNota> Produtos { get; set; }
+
+        public InputNota()
+        {
+            this.Produtos = new List<InputProdutoNota>();
+        }
+
+        public void GravarProduto(InputProdutoNota inputProduto)
+        {
+            if (!this.Produtos.Contains(inputProduto))
+            {
+                this.Produtos.Add(inputProduto);
+            }
+        }
+
+        public void RemoverProduto(InputProdutoNota produto)
+        {
+            this.Produtos.Remove(produto);
+        }
+
+        public void RemoverTodos()
+        {
+            this.Produtos.Clear();
+        }
+    }
+}
---
+++ CursoSoftBlue/Treinojunior/ProjectModel/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinojunior.ProjectModel
+{
+    class Produto
+    {
+        public Guid ID { get; set; }
+        public string Descricao { get; set; }
+        public double PrecoCusto { get; set; }
+        public double PrecoVenda { get; set; }
+        public double Estoque { get; set; }
+
+    }
+}

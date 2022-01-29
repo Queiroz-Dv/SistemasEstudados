3fe1d0c444785b4d81298a3f58722708be742f4f
Queiroz <kalifor_dbel@hotmail.com>
2022-01-28 21:56:36 -0300

    Implementação das tabelas no banco de dados finalizadas.

---
+++ CursoSoftBlue/Treinojunior/WindowsFormsSistemas/DataSetTipado/DataSets/dbo.NotaDeEntrada.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[NotadeEntrada]
+(
+	[idnotaentrada] BIGINT NOT NULL PRIMARY KEY, 
+    [idfornecedor] BIGINT NOT NULL, 
+    [numerodanota] NVARCHAR(15) NOT NULL, 
+    [dataemissao] DATE NOT NULL, 
+    [dataentrada] DATE NOT NULL, 
+    CONSTRAINT [FK_NotadeEntrada_Fornecedores]
+	FOREIGN KEY ([idfornecedor]) REFERENCES [Fornecedores]([idfornecedor])
+)
---
+++ CursoSoftBlue/Treinojunior/WindowsFormsSistemas/DataSetTipado/DataSets/dbo.ProdutosNotaDeEntrada.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ProdutosNotaDeEntrada] (
+    [idprodutonotaentrada] BIGINT          NOT NULL,
+    [idnotadeentrada]      BIGINT          NOT NULL,
+    [idproduto]            BIGINT          NOT NULL,
+    [precocustocompra]     NUMERIC (18, 2) NOT NULL,
+    [quantidadecomprada]   NUMERIC (18, 3) NOT NULL,
+    PRIMARY KEY CLUSTERED ([idprodutonotaentrada] ASC),
+    CONSTRAINT [FK_ProdutosNotaDeEntrada_NotadeEntrada] FOREIGN KEY ([idnotadeentrada]) REFERENCES [dbo].[NotadeEntrada] ([idnotaentrada]), 
+    CONSTRAINT [FK_ProdutosNotaDeEntrada_Produtos] FOREIGN KEY ([idproduto]) REFERENCES [Produtos]([idproduto])
+);
+

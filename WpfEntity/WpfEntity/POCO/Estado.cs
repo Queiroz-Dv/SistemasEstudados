f6c662d5e120dcd15617a169e3e55f9b80b774b5
Queiroz <kalifor_dbel@hotmail.com>
2022-01-31 07:18:21 -0300

    Novo projeto wpf iniciado, EF adicionado ao projeto.

---
+++ WpfEntity/WpfEntity/POCO/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEntity.POCO
+{
+    public class Estado
+    {
+        public long Id { get; set; }
+        public string UF { get; set; }
+        public string Nome { get; set; }
+    }
+}
